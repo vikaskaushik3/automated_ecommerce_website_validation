434e47a1041d3e8c577114e25d34dafcc2af52ef
Vikaas <vikaskaushik166@gmail.com>
2025-03-18 17:06:28 +0530

    updated chrome driver code in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="module", autouse=True)
 def web_driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--no-sandbox")  # Required for some CI environments
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Prevent memory issues
+    driver = webdriver.Chrome(options=chrome_options)
+    # driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
     driver.quit()
